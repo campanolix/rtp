91b1537ddbe8d11242c3a0515aa9ec0769d9f8bf
Xeno Campanoli <campanolix@rhapsody.com>
2012-01-10 10:49:39 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtp::Application.config.secret_token = '30ff5d1ac23a0aa92da3ca509781ad810ecdc2b15d60b8758e361871d1801752e8dbf3cfc599b623b89d04dcb830ad4b8d8577a657ca2139f9428bbcd726bc02'
